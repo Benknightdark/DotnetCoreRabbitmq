f5319b23505a25838ba7925d9399f92e183bee2f
BenKung <moon791017@gmail.com>
2019-09-02 21:14:49 +0800

    OK

---
+++ Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer {
+    class Program {
+        static void Main (string[] args) {
+            var factory = new ConnectionFactory ();
+
+            factory.UserName = ("rabbitmq");
+            factory.Password = ("rabbitmq");
+            factory.HostName = ("localhost");
+            factory.Port = (5672);
+            using (var connection = factory.CreateConnection ())
+            using (var chanel = connection.CreateModel ()) {
+                chanel.QueueDeclare ("basic", false, false, false, null);
+                var body = Encoding.UTF8.GetBytes ("aaaaa");
+                chanel.BasicPublish ("", "basic", null, body);
+            }
+            Console.WriteLine ("科");
+            Console.ReadLine ();
+        }
+    }
+}
