d1b162cae76e350bd4ea69b9866abb6e323c81a6
Ben Kung <ben_kung@infofab.com>
2019-09-03 09:01:13 +0800

    add Event Bus infra

---
+++ Domain/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public class Event
+    {
+        public DateTime TimeStamp{get;protected set;}
+        protected Event(){
+            TimeStamp=DateTime.Now;
+        }
+    }
+}
